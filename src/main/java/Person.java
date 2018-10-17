1d6fb44c4577dec39a49a7f728d8da3f50d27eb1
devOpElliot <efortney@missouriwestern.edu>
2018-10-16 20:24:19 -0500

    Added a full details method to Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,19 +1,16 @@
 /**
  * This is the person class that all actors of the system will inherit from.
  * It provides basic functionality related to personal data.
- *
  */
 public class Person {
 
-    String firstName;
-    String lastName;
-    String zip;
-    String streetAddress;
-    String city;
-    String state;
-    String SSN;
+    String firstName, lastName;
 
-    public Person (String firstName, String lastName, String SSN, String zip, String streetAddress, String city, String state) {
+    String streetAddress, city, state;
+
+    int SSN, zip;
+
+    Person(String firstName, String lastName, int SSN, int zip, String streetAddress, String city, String state) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.SSN = SSN;
@@ -23,16 +20,23 @@ public class Person {
         this.state = state;
     }
 
-
+    // print the full name of a person leading with last name
     String fullName() {
-        return String.format("%s, %s",lastName, firstName);
+        return String.format("%s, %s", lastName, firstName);
     }
 
 
+    // print all details about a user
+    String fullDetails() {
+        return String.format("First: %s \n Last: %s \n SSN: %d \n Street: %s \n Zip: %d \n" +
+                " City: %s \n State: %s \n", firstName, lastName, SSN, streetAddress, zip, city, state
+        );
+    }
+
+
+    // print the full address of a person
     String fullAddress() {
         return String.format("%s, %s, %s", streetAddress, city, state);
     }
-
-
 
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 public class PersonTest {
 
-    Person testPerson = new Person("Joe","Schmoe","123123123","64454","123 Rochester Lane","Saint Joseph", "MO");
+    private Person testPerson = new Person("Joe","Schmoe",123123123,64454,"123 Rochester Lane","Saint Joseph", "MO");
 
     @Test
     public void fullAddressTest() throws Exception {
         assertEquals("123 Rochester Lane, Saint Joseph, MO", testPerson.fullAddress());
+    }
+
+    @Test
+    public void fullDetialsTest() throws Exception {
+        assertEquals(String.format("First: %s \n Last: %s \n SSN: %d \n Street: %s \n Zip: %d \n" +
+                " City: %s \n State: %s \n", testPerson.firstName, testPerson.lastName, testPerson.SSN,
+                testPerson.streetAddress, testPerson.zip, testPerson.city, testPerson.state
+        ), testPerson.fullDetails());
     }
 
     @Test
