25c28a64e8021379d146fc23e97f7a4350b381bc
searlda <spenfraz@gmail.com>
2018-10-22 17:49:44 -0500

    changes to test file

--- src/test/java/BankingApplicationTest.java
+++ src/test/java/BankingApplicationTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BankingApplicationTest {
-    @Test
+    @Test //tests
     public void testMethod() throws Exception {
         assertEquals(5, BankingApplication.testMethod());
     }
