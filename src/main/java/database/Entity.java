8c9fa18caf3410de9fc3be0700572c3fde59c74f
searlda <spenfraz@gmail.com>
2018-10-27 20:14:20 -0500

    (Entity.java) added getMemberFields()

--- src/main/java/database/Entity.java
+++ src/main/java/database/Entity.java
@@ -22,5 +22,54 @@ public class Entity
   public String getBackupTextFileName(){ return ""; }
   public String getDelimiter(){ return ", "; }
 
+
+  //get all member variables (name, type and value) for Calling (subclass Entity) class 
+  public ArrayList<FieldNameTypeAndValue> getMemberFields() throws NoSuchFieldException,InstantiationException,IllegalAccessException
+  {
+    //FieldNameAndType is object with each field and its corresponding type
+    //obtained through reflection/introspection
+    ArrayList<FieldNameTypeAndValue> fields = new ArrayList<FieldNameTypeAndValue>();
+    
+    //java.lang.reflect.Field  :  reflective/introspective call
+    for(Field field : this.getClass().getDeclaredFields())
+    {
+      String type;
+
+      //field.getType() ==> "int" (or) "long" (or) "double" (primitive)
+      if(field.getType().toString().split(" ").length == 1)
+      {
+        fields.add(new FieldNameTypeAndValue(field.getName(),field.getType().toString()));
+      }
+      
+      else //field.getType() ==> "class java.lang.String" ==> "java.lang.String" ==> "String"
+      {
+        //field.getType() ==>  ["class","java.lang.String"] ==> "java.lang.String"
+        type = field.getType().toString().split(" ")[1];
+        //"java.lang.String" ==> ["java","lang","String"] ==> "String"
+        fields.add(new FieldNameTypeAndValue(field.getName(),type.split("\\.")[2]));
+      }
+    }//end of for
+
+    //add in values to FieldNameTypeAndValue
+    for(FieldNameTypeAndValue field : fields)
+    {
+       //loop through this classes methods to find getter for this field
+       for(Method method : this.getClass().getDeclaredMethods())
+       { 
+         //split on space and grab the 3rd element (method name)
+         String methodHead = method.toString().split(" ")[2];
+         if(methodHead.contains("get") && methodHead.contains(field.getName()))
+         {
+           try{
+             field.setValue(method.invoke(this));
+           }catch(InvocationTargetException e){ e.printStackTrace(); }
+         }//end of if
+       }//end of for
+         
+     }//end of for
+
+    return fields;
+  }//end of getMemberFields
+
   
 }//end of Entity class
