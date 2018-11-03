46bab627cc4235ca0a557bf7ba7d8ce6819bb53e
searlda <spenfraz@gmail.com>
2018-11-03 01:53:41 -0500

    (AccountEntity.java) added getters and setters, created db/Account/ folder, created Account.txt and OLD_Account.txt

--- src/main/java/database/Account/AccountEntity.java
+++ src/main/java/database/Account/AccountEntity.java
@@ -62,4 +62,29 @@ public class AccountEntity extends Entity
 
     }//end of parsing AccountEntity constructor
 
+
+    //these won't change, and we don't want them getting returned with other fields
+    //from calls to getFields()
+    public String getTextFileName(){ return "./db/Account/Account.txt"; }
+    public String getBackupTextFileName(){ return "./db/Account/OLD_Account.txt"; }
+    public String getDelimiter(){ return ", "; }
+
+    public long   getID(){ return ID; }
+    public String getSSN(){ return SSN; }
+    public double getBALANCE(){ return BALANCE; }
+    public String getTYPE(){ return TYPE; }
+    public String getSUBTYPE(){ return SUBTYPE; }
+    public String getOVERDRAFT_PROTECT(){ return OVERDRAFT_PROTECT; }
+    public long   getOVERDRAFT_ACCOUNT_ID(){ return OVERDRAFT_ACCOUNT_ID; }
+    public String getDATE_CREATED(){ return DATE_CREATED; }
+    public double getINTEREST_RATE(){ return INTEREST_RATE; }
+    public double getMINIMUM_PAYMENT(){ return MINIMUM_PAYMENT; }
+    public String getDATE_NEXT_PAYMENT_DUE(){ return DATE_NEXT_PAYMENT_DUE; }
+
+    public void setBALANCE(double amount){ BALANCE = amount; }
+    public void setOVERDRAFT_PROTECT(String status){ OVERDRAFT_PROTECT = status; }
+    public void setOVERDRAFT_ACCOUNT_ID(long id){ OVERDRAFT_ACCOUNT_ID = id; }
+    public void setINTEREST_RATE(double rate){ INTEREST_RATE = rate; }
+    public void setMINIMUM_PAYMENT(double amount){ MINIMUM_PAYMENT = amount; }
+    public void setDATE_NEXT_PAYMENT_DUE(String date){ DATE_NEXT_PAYMENT_DUE = date; }
 }
